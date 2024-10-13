b1b82439ed783d3d4f7f7156ec90fbe3981eed82
Marcos Willian <willianmw@hotmail.com>
2024-10-13 20:43:26 -0300

    changes for gh_pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Marcosmwx', // Usually your GitHub org/user name.
+  organizationName: 'marcosmwx', // Usually your GitHub org/user name.
   projectName: 'wiki_ft', // Usually your repo name.
 
   onBrokenLinks: 'throw',
