758a0a57bb219714d08f2ef1d1cf6f355c69ff04
Marcos Willian <willianmw@hotmail.com>
2024-10-13 20:03:33 -0300

    mudancas para ignorar um tipo a mais de pasta .idea

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,14 +13,14 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://marcosmwx.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'Marcosmwx', // Usually your GitHub org/user name.
   projectName: 'wiki_ft', // Usually your repo name.
 
   onBrokenLinks: 'throw',
@@ -30,8 +30,8 @@ const config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'pt',
+    locales: ['pt'],
   },
 
   presets: [
